f407f2b513c0cbb026c90c1b44698120ba2f4340
Sebastian Esquivel <set1.esq@gmail.com>
2023-08-11 18:13:11 -0400

    Interfaz de login realizada

--- llapemn_frontend/src/components/Admin/LoginForm/LoginForm.jsx
+++ llapemn_frontend/src/components/Admin/LoginForm/LoginForm.jsx
@@ -5,6 +5,7 @@ import * as Yup from "yup";
 import { toast } from "react-toastify";
 import { useAuth } from "../../../hooks";
 
+let url = "https://www.llapemn.cl/images/logoBlanco.png";
 export function LoginForm() {
   const { login } = useAuth();
   console.log(useAuth());
@@ -23,45 +24,53 @@ export function LoginForm() {
     },
   });
   return (
-    <div className="mb-8 flex justify-center">
+    <div className="mb-8 flex justify-center font-leca">
       <form onSubmit={formik.handleSubmit}>
-        <h1 className="mb-6 text-center text-6xl text-white">Iniciar Sesión</h1>
-        <h3 className="mb-6 text-center text-white">¡Bienvenido/a de nuevo!</h3>
-        <div className="mb-6">
-          <input
-            name="email"
-            placeholder="Correo electronico"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            className="w-full rounded-lg  bg-[#B7C05F] px-4  py-2 placeholder-white  focus:outline-none focus:ring-2 focus:ring-cyan-500"
-          />
-        </div>
-        <div className="mb-6">
-          <input
-            name="password"
-            type="password"
-            placeholder="Contraseña"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            className="w-full rounded-lg bg-[#B7C05F]  px-4 py-2 placeholder-white focus:outline-none focus:ring-2 focus:ring-cyan-500"
-          />
+        <img src={url} className="mx-auto w-1/3 " />
 
-          <div className="mt-4 flex w-full items-baseline justify-between">
-            <label className="flex items-center text-sm text-white">
-              <input type="checkbox" className="mr-2 accent-[#59167F]" />
-              Recordar
-            </label>
-            <a className="mt-2 block text-right text-sm  text-white">
-              ¿Olvidaste tu contraseña?
-            </a>
-          </div>
-        </div>
-        <button
-          type="submit"
-          className="mx-auto mb-6 mt-4 block w-full rounded-lg bg-[#59167F] py-2 text-white focus:outline-none focus:ring-2 "
-        >
+        <h1 className="mb-6 text-center font-leca text-5xl text-white">
           Iniciar Sesión
-        </button>
+        </h1>
+        <h3 className="mb-6 text-center font-leca text-white">
+          ¡Bienvenido/a de nuevo!
+        </h3>
+        <div className="px-16">
+          <div className="mb-6 ">
+            <input
+              name="email"
+              placeholder="Correo electronico"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              className="w-full rounded-lg  bg-[#B7C05F] px-4  py-2 placeholder-white  focus:outline-none focus:ring-2 focus:ring-[#59167F]"
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              name="password"
+              type="password"
+              placeholder="Contraseña"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              className="w-full rounded-lg bg-[#B7C05F]  px-4 py-2 placeholder-white focus:outline-none focus:ring-2 focus:ring-[#59167F]"
+            />
+
+            <div className="mt-4 flex w-full items-baseline justify-between">
+              <label className="flex items-center text-sm text-white">
+                <input type="checkbox" className="mr-2 accent-[#59167F]" />
+                Recordar
+              </label>
+              <a className="mt-2 block text-right text-sm  text-white">
+                ¿Olvidaste tu contraseña?
+              </a>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="mx-auto mb-6 mt-4 block w-full rounded-lg bg-[#59167F] py-2 text-white focus:outline-none focus:ring-2 "
+          >
+            Iniciar Sesión
+          </button>
+        </div>
       </form>
     </div>
   );
--- llapemn_frontend/tailwind.config.js
+++ llapemn_frontend/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      leca: ["Lexend Deca", " sans-serif"],
+    },
   },
   plugins: [],
 };
