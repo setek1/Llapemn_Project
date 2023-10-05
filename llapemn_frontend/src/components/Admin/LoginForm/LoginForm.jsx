59d094c63102c14308b34e884d7058c014bc25be
Sebastian Esquivel <set1.esq@gmail.com>
2023-10-05 17:54:36 -0300

    Inputs Errors

--- llapemn_frontend/src/components/Admin/LoginForm/LoginForm.jsx
+++ llapemn_frontend/src/components/Admin/LoginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { loginApi } from "../../../api/user";
-import { useFormik } from "formik";
+import { useFormik, ErrorMessage, FormikProvider } from "formik";
 import * as Yup from "yup";
 import { toast } from "react-toastify";
 import { useAuth } from "../../../hooks";
@@ -12,6 +12,7 @@ export function LoginForm() {
   const formik = useFormik({
     initialValues: intialValues(),
     validationSchema: Yup.object(validationSchema()),
+    // validateOnChange: false,
 
     onSubmit: async (formValue) => {
       try {
@@ -24,55 +25,77 @@ export function LoginForm() {
     },
   });
   return (
-    <div className="mb-8 flex justify-center font-leca">
-      <form onSubmit={formik.handleSubmit}>
-        <img src={url} className="mx-auto w-1/3  " />
+    <FormikProvider value={formik}>
+      <div className="mb-8 flex justify-center font-leca">
+        <form onSubmit={formik.handleSubmit}>
+          <img src={url} className="mx-auto w-1/3  " />
 
-        <h1 className="mb-6 text-center font-leca text-5xl text-white">
-          Iniciar Sesión
-        </h1>
-        <h3 className="mb-6 text-center font-leca text-white">
-          ¡Bienvenido/a de nuevo!
-        </h3>
-        <div className="px-16">
-          <div className="mb-6 ">
-            <input
-              name="email"
-              placeholder="Correo electronico"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              className="w-full rounded-lg  bg-[#B7C05F] px-4  py-2 placeholder-white  focus:outline-none focus:ring-2 focus:ring-[#59167F]"
-            />
-          </div>
-          <div className="mb-6">
-            <input
-              name="password"
-              type="password"
-              placeholder="Contraseña"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              className="w-full rounded-lg bg-[#B7C05F]  px-4 py-2 placeholder-white focus:outline-none focus:ring-2 focus:ring-[#59167F]"
-            />
-
-            <div className="mt-4 flex w-full items-baseline justify-between">
-              <label className="flex items-center text-sm text-white">
-                <input type="checkbox" className="mr-2 accent-[#59167F]" />
-                Recordar
-              </label>
-              <a className="mt-2 block text-right text-sm  text-white">
-                ¿Olvidaste tu contraseña?
-              </a>
-            </div>
-          </div>
-          <button
-            type="submit"
-            className="mx-auto mb-6 mt-4 block w-full rounded-lg bg-[#59167F] py-2 text-white focus:outline-none focus:ring-2 "
-          >
+          <h1 className="mb-6 text-center font-leca text-5xl text-white">
             Iniciar Sesión
-          </button>
-        </div>
-      </form>
-    </div>
+          </h1>
+          <h3 className="mb-6 text-center font-leca text-white">
+            ¡Bienvenido/a de nuevo!
+          </h3>
+          <div className="px-16">
+            <div className="mb-6 ">
+              <input
+                name="email"
+                placeholder="Correo electronico"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                className={` bg-[#B7C05F] ${
+                  formik.touched.email && formik.errors.email
+                    ? "border-red-500 focus:ring-red-500"
+                    : "focus:ring-[#59167F]"
+                } w-full rounded-lg  bg-[#B7C05F] px-4  py-2 placeholder-white  focus:outline-none focus:ring-2 `}
+                // "w-full rounded-lg  bg-[#B7C05F] px-4  py-2 placeholder-white  focus:outline-none focus:ring-2 focus:ring-[#59167F]"
+              />
+              <ErrorMessage
+                name="email"
+                className="text-red-700"
+                component="div"
+              />
+            </div>
+
+            <div className="mb-6">
+              <input
+                name="password"
+                type="password"
+                placeholder="Contraseña"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                className={` bg-[#B7C05F] ${
+                  formik.touched.password && formik.errors.password
+                    ? "border-red-500 focus:ring-red-500"
+                    : "focus:ring-[#59167F]"
+                } w-full rounded-lg  bg-[#B7C05F] px-4  py-2 placeholder-white  focus:outline-none focus:ring-2 `}
+              />
+              <ErrorMessage
+                name="password"
+                className="text-red-700"
+                component="div"
+              />
+
+              <div className="mt-4 flex w-full items-baseline justify-between">
+                <label className="flex items-center text-sm text-white">
+                  <input type="checkbox" className="mr-2 accent-[#59167F]" />
+                  Recordar
+                </label>
+                <a className="mt-2 block text-right text-sm  text-white">
+                  ¿Olvidaste tu contraseña?
+                </a>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="mx-auto mb-6 mt-4 block w-full rounded-lg bg-[#59167F] py-2 text-white focus:outline-none focus:ring-2 "
+            >
+              Iniciar Sesión
+            </button>
+          </div>
+        </form>
+      </div>
+    </FormikProvider>
   );
 }
 
@@ -85,7 +108,13 @@ function intialValues() {
 
 function validationSchema() {
   return {
-    email: Yup.string().email(true).required(true),
-    password: Yup.string().required(true),
+    email: Yup.string()
+      .email("Ingrese un Correo Valido")
+      .required("Porfavor Ingrese un Correo")
+      .matches(
+        /^[a-zA-Z0-9@._-]+$/,
+        "El correo electrónico no puede contener signos",
+      ),
+    password: Yup.string().required("Ingrese una contraseña"),
   };
 }
